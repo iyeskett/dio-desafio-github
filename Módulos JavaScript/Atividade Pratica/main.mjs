c4ff79825e00b026a8b2c506c458d975bc72f325
iyeskett <felipesilva.mello27@gmail.com>
2022-06-23 15:25:18 -0300

    Módulos JavaScript

---
+++ Módulos JavaScript/Atividade Pratica/main.mjs
@@ -0,0 +1,5 @@
+import { mostraIdade, mostraCidade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Felipe", 20));
+console.log(mostraCidade("Felipe", "São Paulo"));
+console.log(mostraHobby("Felipe", "estudar programação"));
